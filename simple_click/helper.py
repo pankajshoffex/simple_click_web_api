4898b07e09ec87eb757f64e5fb3855e41f30166a
Pankaj Badgujar <pankajbadgujar@Pankaj-Badgujar.local>
2019-06-08 22:19:19 +0530

    new feature added

--- simple_click/helper.py
+++ simple_click/helper.py
@@ -43,5 +43,5 @@ def get_today_range(is_past_date=False, is_tomorrow_date=False):
         today_min = datetime.datetime.combine(datetime.date.today() - timedelta(days=1), datetime.time.min)
     today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
     if is_tomorrow_date:
-        today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.max + timedelta(hours=7))
+        today_max = datetime.datetime.combine(datetime.date.today() + timedelta(hours=7), datetime.time.max)
     return [today_min, today_max]
